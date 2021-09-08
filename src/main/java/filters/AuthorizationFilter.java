5fd492dadbee8869c615102caaa07b6a86198247
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-09-08 12:51:55 +0200

    Create class AuthorizationFilter

---
+++ src/main/java/filters/AuthorizationFilter.java
@@ -0,0 +1,46 @@
+package filters;
+
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class AuthorizationFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+
+        String login = request.getParameter("name");
+        String password = request.getParameter("password");
+
+
+        if ( (login == null && password == null) || (login.length() > 3
+                && password.length() > 5 && findExpression("[0-9]", password) ) ) {
+
+            chain.doFilter(request, response);
+
+        } else {
+            request.getRequestDispatcher("ex_error.jsp").forward(request, response);
+        }
+
+
+    }
+
+    private boolean findExpression(String expression, String text) {
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find();
+    }
+
+    public void destroy() {
+
+    }
+
+}
